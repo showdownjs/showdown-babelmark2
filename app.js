87df402bbe60676b388670f276303d013eda35c1
Estevao Soares dos Santos <estevao.santos@gmail.com>
2017-02-02 01:50:56 +0000

    initial commit

--- app.js
+++ app.js
@@ -1,43 +1,72 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+var contentTypes    = require('./utils/content-types'),
+    sysInfo         = require('./utils/sys-info'),
+    fs              = require('fs'),
+    env             = process.env,
+    express         = require('express'),
+    app             = express(),
+    showdown        = require('showdown'),
+    showdownVersion = JSON.parse(fs.readFileSync('./node_modules/showdown/package.json', 'utf8')).version,
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
+    port            = env.NODE_PORT || 3000,
+    host            = env.NODE_IP || 'localhost';
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
+app.get('/', function (req, response) {
+  response.json({
+    title: 'showdown test API',
+    api: {
+      vanilla: '/vanilla?text=',
+      github: '/github?text='
+    }
+  })
+});
 
-  if (url == '/health') {
-    res.writeHead(200);
-    res.end();
-  } else if (url == '/info/gen' || url == '/info/poll') {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+app.get('/health', function (req, response) {
+  response.status(200);
+  response.end();
+});
+
+app.get('/vanilla', function (request, response, next) {
+  var converter = new showdown.Converter();
+  var text = request.query.text;
+
+  response.type('json');
+
+  if (!text || text == '') {
+    response.status(400).json({error: 'No text sent'});
+  } else if (text.length > 1000) {
+    response.status(400).json({error: 'Text > 1000 characters'});
   } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end('Not found');
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
+    converter.setFlavor('vanilla');
+    var html = converter.makeHtml(text);
+    response.json({
+      name: 'showdown',
+      html: html,
+      version: showdownVersion
     });
   }
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
-  console.log(`Application worker ${process.pid} started...`);
+app.get('/github', function (request, response, next) {
+  var converter = new showdown.Converter();
+  var text = request.query.text;
+
+  response.type('json');
+
+  if (!text || text == '') {
+    response.status(400).json({error: 'No text sent'});
+  } else if (text.length > 1000) {
+    response.status(400).json({error: 'Text > 1000 characters'});
+  } else {
+    converter.setFlavor('github');
+    var html = converter.makeHtml(text);
+    response.json({
+      name: 'showdown (flavor: github)',
+      html: html,
+      version: showdownVersion
+    });
+  }
+});
+
+app.listen(port, host, function () {
+  console.log('Application worker ' + process.pid + ' started on port ' + port + '...');
 });
--- start.js
+++ start.js
@@ -1,11 +1,11 @@
-const cluster = require('cluster'),
+var cluster = require('cluster'),
       stopSignals = [
         'SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
       ],
       production = process.env.NODE_ENV == 'production';
 
-let stopping = false;
+var stopping = false;
 
 cluster.on('disconnect', function(worker) {
   if (production) {
@@ -19,14 +19,14 @@ cluster.on('disconnect', function(worker) {
 
 if (cluster.isMaster) {
   const workerCount = process.env.NODE_CLUSTER_WORKERS || 4;
-  console.log(`Starting ${workerCount} workers...`);
-  for (let i = 0; i < workerCount; i++) {
+  console.log('Starting ' + workerCount + 'workers...');
+  for (var i = 0; i < workerCount; i++) {
     cluster.fork();
   }
   if (production) {
     stopSignals.forEach(function (signal) {
       process.on(signal, function () {
-        console.log(`Got ${signal}, stopping workers...`);
+        console.log('Got ' + signal + ', stopping workers...');
         stopping = true;
         cluster.disconnect(function () {
           console.log('All workers stopped, exiting.');
